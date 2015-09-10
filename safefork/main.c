15df544555f7ea4c11d050082fda97ab84538f53
Greb Nafets <stefan.berg85@gmail.com>
2015-09-10 22:08:07 +0000

    "safefork" created.
     An attempt to create fork safty via concept of semaphores.
     The plan is to tell all nree(Non-RE-Entry) functions to wait if fork is
     running and vice versa.
    
     So far: [malloc, realloc, calloc and free] have been added as nree.
    
     Changes to be committed:
    	new file:   safefork/main
    	new file:   safefork/main.c
    	new file:   safefork/makefile
    	new file:   safefork/safefork.h

---
+++ safefork/main.c
@@ -0,0 +1,79 @@
+#define TEST_NO_RESULT_THEN_SHHH
+#include "../test/test.h"
+
+/* WARNING. Although fork is tested this way, you should be careful of forking
+ * within a thread or within a fork. Not saying you can't or shouldn't, I am
+ * just saying your error correction tools might bite you a bit later..
+ * I know fork only copies page on write, still...
+ * */
+
+/* Testing fork safty is hard, so I basically cheated by checking if
+ * spinlock did run and if the program exited normally. (That means no
+ * spinlock.)
+ * Sooo...
+ * 	step 1. Make forks and nrees run longer with SAFEFORK_SPIN.
+ * 	step 2. In thread A -> run bunch of reallocs.
+ * 	step 3. In thread B -> run bunch of threads.
+ * 	step 4. Hope both spinlocks will run.....
+ * 	step 5. Stand up and turn around 2 times.
+ * 	step 6. Sit down and check the results. If the program is still
+ * 		running, then you have a spinlock. Patch it, then stand up
+ * 		again and spin twice but now in the other direction.
+ * */
+
+long ffork_wait = 0;
+long fnree_wait = 0;
+
+#define SAFEFORK_HOOK
+#define NREE_SPIN fnree_wait++
+#define FORK_SPIN ffork_wait++
+#define SAFEFORK_SPIN usleep(1)
+
+#include "safefork.h"
+
+pthread_t t[2];
+
+void end_threads()
+{
+	pthread_join(t[0], NULL);
+	pthread_join(t[1], NULL);
+}
+
+void *t1(void *arg __attribute__((unused)))
+{
+	size_t i, len;
+	void *ptr = NULL;
+	len = 5;
+	for (i = 0; i < len; i += 1) {
+		ptr = realloc(ptr, i);
+		usleep(1);
+	}
+	free(ptr);
+	return NULL;
+}
+
+void *t2(void *arg __attribute__((unused)))
+{
+	size_t len;
+	len = 5;
+	while (len--) {fork(); usleep(1);}
+	fork_cleanup();
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	size_t mode = test_get_mode_from_arg(argc, argv);
+	test_set(mode);
+
+	pthread_create(&t[0], NULL, &t1, NULL);
+	pthread_create(&t[1], NULL, &t2, NULL);
+	end_threads();
+
+	/* If these values are more then 0, then at some point there was a 
+	 * lock but it didn't end in a spinlock. */
+	test(ffork_wait > 0);
+	test(fnree_wait > 0);
+
+	return EXIT_SUCCESS;
+}
---
+++ safefork/safefork.h
@@ -0,0 +1,234 @@
+/* nree = Non-RE-Entry (method)
+ *
+ * This header file is far from complete..
+ * Only malloc, calloc, realloc and free have been made "safe" so far.
+ * It is _NOT_ enough to just include the header file, you have to tell
+ * it to overwrite the functions first.
+ * You currently can overwrite:
+ * [fork, malloc, realloc, calloc and free] by defining "SAFEFORK_HOOK" macro.
+ * That will hook target -> with:
+ * fork -> fork_safe
+ * [malloc, calloc, realloc, free] -> nree_*[malloc, calloc, realloc, free]
+ *
+ * "safefork.h" provides you with lockfork, unlockfork, locknree, unlocknree,
+ * nree_wait and nree_done functions. The _wait and _done functions will
+ * automatically call lock and unlock. forklock will only work for the
+ * function fork_safe, so you might want to hook it if you didn't or don't
+ * want to define the "SAFEFORK_HOOK" macro. See nree_malloc below to see
+ * these functions in use.
+ * */
+
+#ifndef SAFEFORK
+#define SAFEFORK
+#define FORK
+#define NREE
+
+#include "../support/support.h"
+
+#ifdef CAN_FORK
+
+/* include {{{ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include "../badcontext/badcontext.h"
+/* }}} */
+
+/* Profile layer {{{ */
+
+#ifndef PS
+	#define PS
+#endif /* PS */
+#ifndef PE
+	#define PE
+#endif /* PE */
+
+/* }}} */
+
+/* context {{{1 */
+
+/* bad {{{2 */
+#define SAFEFORK_FORK_CLEANUP 2
+const int CONTEXT_BAD_FAILED_TO_CREATE_FORK = __COUNTER__;
+/* }}}2 */
+
+/* }}}1 */
+
+/* spin locks {{{ */
+#ifndef FORK_SPIN
+	#define FORK_SPIN
+#endif /* FORK_LOCK */
+
+#ifndef NREE_SPIN
+	#define NREE_SPIN
+#endif /* NREE_LOCK */
+
+#ifndef SAFEFORK_SPIN
+	#define SAFEFORK_SPIN
+#endif /* SAFEFORK_SPIN */
+
+/* }}} */
+
+long lock_fork = 0;
+long lock_nree = 0;
+
+void lockfork()
+{
+	lock_fork++;
+}
+void unlockfork()
+{
+	lock_fork--;
+}
+void locknree()
+{
+	lock_nree++;
+}
+void unlocknree()
+{
+	lock_nree--;
+}
+
+/* fork {{{ */
+void fork_wait()
+{
+	PS;
+	while (lock_fork > 0) {FORK_SPIN;}
+	locknree();
+	PE;
+}
+
+void fork_done()
+{
+	PS;
+	unlocknree();
+	PE;
+}
+
+pid_t fork_safe()
+{
+	PS;
+	fork_wait();
+	SAFEFORK_SPIN;
+	pid_t id = fork();
+	if (id == -1) {
+		cntxt = CONTEXT_BAD_FAILED_TO_CREATE_FORK;
+		bad   = BAD;
+	}
+	PE;
+	return id;
+}
+
+void fork_atfork() __attribute__((constructor));
+void fork_atfork()
+{
+	PS;
+	pthread_atfork(NULL, &fork_done, &fork_done);
+	PE;	
+}
+
+void fork_cleanup()
+{
+	PS;
+	int status;
+	pid_t w;
+	for (;;) {
+		w = wait(&status);
+		if (w == -1 && errno == ECHILD) {break;}
+		if (WIFEXITED(status) == 0 || WEXITSTATUS(status) != 0) {
+			cntxt = status;
+			bad   = BAD + SAFEFORK_FORK_CLEANUP;
+			break;
+		}
+	}
+	PE;
+}
+
+/* }}} */
+
+/* nree {{{ */
+
+void nree_wait()
+{
+	PS;
+	while(lock_nree > 0) {NREE_SPIN;}
+	lockfork();
+	PE;
+}
+
+void nree_done()
+{
+	PS;
+	unlockfork();
+	PE;
+}
+
+void *nree_malloc(size_t size)
+{
+	PS;
+	void *ptr = NULL;
+	nree_wait();
+	ptr = malloc(size);
+	SAFEFORK_SPIN;
+	nree_done();
+	PE;
+	return ptr;
+}
+
+void *nree_calloc(size_t unit_count, size_t unit_size)
+{
+	PS;
+	void *ptr = NULL;
+	nree_wait();
+	ptr = calloc(unit_count, unit_size);
+	nree_done();
+	PE;
+	return ptr;	
+}
+
+void *nree_realloc(void *ptr, size_t size)
+{
+	PS;
+	nree_wait();
+	ptr = realloc(ptr, size);
+	SAFEFORK_SPIN;
+	nree_done();
+	PE;
+	return ptr;
+}
+
+void nree_free(void *ptr)
+{
+	PS;
+	nree_wait();
+	free(ptr);
+	SAFEFORK_SPIN;
+	nree_done();
+	PE;
+}
+
+/* }}} */
+
+/* hooks {{{ */
+#ifdef SAFEFORK_HOOK
+
+	#define fork() fork_safe()
+	#define malloc(s) nree_malloc(s)
+	#define calloc(n, s) nree_calloc(n, s)
+	#define realloc(p, s) nree_realloc(p, s)
+	#define free(p) nree_free(p)
+
+#endif /* SAFEFORK_HOOK */
+/* }}} */
+
+/* Undefine profile layer {{{ */
+
+#undef PS
+#undef PE
+
+/* }}} */
+
+#endif /* CAN_FORK */
+#endif /* SAFEFORK */
