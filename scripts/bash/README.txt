f4c82f6cf545212dd14457e713d3979bb9191c3a
Greb Nafets <stefan.berg85@gmail.com>
2015-10-01 19:42:49 +0000

    Reframing and making it easy to reframe again.
     1)	Added scripts and waste direcotries.
    
     2)	Used the scripts to change the framework. Moved cpp into api directory
     	and then moved on to create adjustment directory.
    
     3)	Reconstructing symlinks.
    
     Details can be found @ scripts/bash/foreachdir.sh file.
    
     Changes to be committed:
    	modified:   build/makefile
    	renamed:    cset/assemblyline/cpp/main.cpp -> cset/assemblyline/api/cpp/main.cpp
    	new file:   cset/assemblyline/api/cpp/makefile
    	deleted:    cset/assemblyline/cpp/makefile
    	renamed:    cset/badcontext/cpp/main.cpp -> cset/badcontext/api/cpp/main.cpp
    	new file:   cset/badcontext/api/cpp/makefile
    	deleted:    cset/badcontext/cpp/makefile
    	renamed:    cset/charmap/cpp/main.cpp -> cset/charmap/api/cpp/main.cpp
    	new file:   cset/charmap/api/cpp/makefile
    	deleted:    cset/charmap/cpp/makefile
    	renamed:    cset/critical/cpp/main.cpp -> cset/critical/api/cpp/main.cpp
    	new file:   cset/critical/api/cpp/makefile
    	deleted:    cset/critical/cpp/makefile
    	renamed:    cset/f/cpp/main.cpp -> cset/f/api/cpp/main.cpp
    	new file:   cset/f/api/cpp/makefile
    	deleted:    cset/f/cpp/makefile
    	renamed:    cset/hashmap/cpp/hashmap.h -> cset/hashmap/api/cpp/hashmap.h
    	renamed:    cset/hashmap/cpp/main.cpp -> cset/hashmap/api/cpp/main.cpp
    	new file:   cset/hashmap/api/cpp/makefile
    	deleted:    cset/hashmap/cpp/makefile
    	renamed:    cset/logger/cpp/main.cpp -> cset/logger/api/cpp/main.cpp
    	new file:   cset/logger/api/cpp/makefile
    	deleted:    cset/logger/cpp/makefile
    	renamed:    cset/mem/cpp/main.cpp -> cset/mem/api/cpp/main.cpp
    	new file:   cset/mem/api/cpp/makefile
    	deleted:    cset/mem/cpp/makefile
    	renamed:    cset/safefork/cpp/main.cpp -> cset/safefork/api/cpp/main.cpp
    	new file:   cset/safefork/api/cpp/makefile
    	deleted:    cset/safefork/cpp/makefile
    	renamed:    cset/support/cpp/main.cpp -> cset/support/api/cpp/main.cpp
    	new file:   cset/support/api/cpp/makefile
    	deleted:    cset/support/cpp/makefile
    	renamed:    cset/test/cpp/main.cpp -> cset/test/api/cpp/main.cpp
    	new file:   cset/test/api/cpp/makefile
    	deleted:    cset/test/cpp/makefile
    	renamed:    cset/threadwrapp/cpp/main.cpp -> cset/threadwrapp/api/cpp/main.cpp
    	new file:   cset/threadwrapp/api/cpp/makefile
    	deleted:    cset/threadwrapp/cpp/makefile
    	renamed:    cset/ts/cpp/main.cpp -> cset/ts/api/cpp/main.cpp
    	new file:   cset/ts/api/cpp/makefile
    	deleted:    cset/ts/cpp/makefile
    	new file:   scripts/bash/README.txt
    	new file:   scripts/bash/cset
    	new file:   scripts/bash/foreachdir.sh
    	new file:   waste/makefile-old

---
+++ scripts/bash/README.txt
@@ -0,0 +1,2 @@
+Notice that cset here is symbolic to the cset directory. Make sure it
+is right before you run any scripts.
---
+++ scripts/bash/foreachdir.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# Please, once you are done doing whatever with your script, put it below
+# and comment it out. Try to avoid using the rm command.
+# Just move into whatever you want to remove into the wate directory
+#	@ ${CSET_SEARCH_PATH}/waste
+# Double check its contents with ls command and now you can safly use rm ;)
+
+
+
+# -----------------------------------------------------------------------------
+# Creating api directory under each sub under cset.
+#for DIR in cset/*; do
+#	mkdir "${DIR}/api"
+#done
+#------------------------------------------------------------------------------
+#for DIR in cset/*; do
+#	mkdir "${DIR}/adjustment"
+#done
+#------------------------------------------------------------------------------
+# Moving cpp directory into api, this breaks the makefile symbolic link
+#for DIR in cset/*; do
+#	for SUB in ${DIR}/*; do
+#		if [ "${SUB}" = "${DIR}/cpp" ]; then
+#			mv ${SUB} ${DIR}/api
+#		fi
+#	done
+#done
+#------------------------------------------------------------------------------
+#for DIR in cset/*; do
+#	for SUB in ${DIR}/*; do
+#		if [ "${SUB}" = "${DIR}/api" ]; then
+#			echo "Moving ${SUB}/cpp/makefile to ${SUB}/cpp/makefile-old"
+#			mv ${SUB}/cpp/makefile ${SUB}/cpp/makefile-old
+#		fi
+#	done
+#done
+#------------------------------------------------------------------------------
+#for DIR in cset/*; do
+#	for SUB in ${DIR}/*; do
+#		if [ "${SUB}" = "${DIR}/api" ]; then
+#			echo "Creating symlink ${SUB}/cpp/makefile->CSET_SEARCH_PATH/build/cpp/makefile"
+#			ln -s ${CSET_SEARCH_PATH}/build/cpp/makefile ${SUB}/cpp/makefile
+#		fi
+#	done
+#done
+#------------------------------------------------------------------------------
+#for DIR in cset/*; do
+#	for SUB in ${DIR}/*; do
+#		if [ "${SUB}" = "${DIR}/api" ]; then
+#			echo "Moving ${SUB}/cpp/makefile-old into waste"
+#			mv  ${SUB}/cpp/makefile-old ${CSET_SEARCH_PATH}/waste
+#		fi
+#	done
+#done
